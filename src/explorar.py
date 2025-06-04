2f24fa4506555b4f91ef131904d2c43cfe064d74
DeSouzaSR <sandro.fisica@gmail.com>
2025-06-04 16:29:31 -0300

    Iniciei análise exploratória

---
+++ src/explorar.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        Explorar
+# Purpose:
+#
+# Author:      srsouza
+#
+# Created:     04/06/2025
+# Copyright:   (c) srsouza 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+path_pedagogico = r'../data/raw_data/df_es_filtrado.csv'
+df_ped = pd.read_csv(path_pedagogico)
+
+print(df_ped)
